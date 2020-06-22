0af4837bbba82c8d7a73d7f93e77f3b7a02feee3
zannb <zannb@outlook.com>
2020-06-22 20:55:17 +0800

    use face_recognition instead;

--- initialer.py
+++ initialer.py
@@ -3,20 +3,12 @@ import dlib
 import numpy as np
 import cv2
 import os
-
-FACE_RECOGNITION_MODEL_PATH = './dlib_face_recognition_resnet_model_v1.dat'
-SHAPE_PREDICTOR_MODEL_PATH = './shape_predictor_68_face_landmarks.dat'
+import face_recognition
 
 
 class FaceImgProcessor:
-    def __init__(self,
-                 detector=dlib.get_frontal_face_detector(),
-                 predictor=dlib.shape_predictor(SHAPE_PREDICTOR_MODEL_PATH),
-                 face_rec=dlib.face_recognition_model_v1(FACE_RECOGNITION_MODEL_PATH)
-                 ):
-        self.detector = detector
-        self.predictor = predictor
-        self.face_rec = face_rec
+    def __init__(self):
+        pass
 
     @staticmethod
     def delta_h_eye(shape):
@@ -36,23 +28,18 @@ class FaceImgProcessor:
     def delta_w_mouth(shape):
         return shape[63][0] - shape[59][0]
 
-    def get_rects(self, rgb_img):
-        return self.detector(rgb_img, 1)
+    @staticmethod
+    def get_locations(image):
+        return face_recognition.face_locations(image)
 
-    def get_rects_and_shapes(self, rgb_img):
-        rects = self.get_rects(rgb_img)
-        shapes = []
-        for k, rect in enumerate(rects):
-            shapes.append(self.predictor(rgb_img, rect))
-        return rects, shapes
+    @staticmethod
+    def get_face_landmarks(image):
+        return face_recognition.face_landmarks(image)
 
-    def get_rects_and_shapes_and_face_descriptor(self, rgb_img):
-        rects = self.get_rects(rgb_img)
-        shapes = []
-        fds = []
-        for k, rect in enumerate(rects):
-            shape = self.predictor(rgb_img, rect)
-            shapes.append(shape)
-            fd = self.face_rec.compute_face_descriptor(rgb_img, shape)
-            fds.append(fd)
-        return rects, shapes, fds
+    @staticmethod
+    def get_face_encodings(image):
+        return face_recognition.face_encodings(image)
+
+    @staticmethod
+    def compare_faces(target_face_list, face):
+        return face_recognition.compare_faces(target_face_list, face)
--- play_get_68_points with_webcam.py
+++ play_get_68_points with_webcam.py
@@ -3,83 +3,41 @@ import dlib
 import numpy as np
 import cv2
 import os
-
-LEFT_EYE = [37, 38, 41, 40]
-RIGHT_EYS = [43, 44, 47, 46]
-
-FACE_RECOGNITION_MODEL_PATH = './dlib_face_recognition_resnet_model_v1.dat'
-SHAPE_PREDICTOR_MODEL_PATH = './shape_predictor_68_face_landmarks.dat'
-KNOWN_FACE_DIR = './known_face'
-
-detector = dlib.get_frontal_face_detector()
-predictor = dlib.shape_predictor(SHAPE_PREDICTOR_MODEL_PATH)
-face_rec = dlib.face_recognition_model_v1(FACE_RECOGNITION_MODEL_PATH)
-
-
-def delta_h_eye(shape):
-    left = shape[36][1] + shape[37][1] - shape[39][1] - shape[40][1]
-    right = shape[42][1] + shape[43][1] - shape[46][1] - shape[45][1]
-    return left / 2, right / 2
-
-
-def delta_w_eye(shape):
-    return shape[38][0] - shape[35][0]
-
-
-def delta_h_mouth(shape):
-    return (shape[60][1] + shape[61][1] + shape[62][1] - shape[64][1] - shape[65][1] - shape[66][1]) / 3
-
-
-def delta_w_mouth(shape):
-    return shape[63][0] - shape[59][0]
-
-
-def load_known_faces():  # ->type: dict
-    results = {}
-    for each in os.listdir(KNOWN_FACE_DIR):
-        img = dlib.load_rgb_image(f'{KNOWN_FACE_DIR}/{each}')
-        fd = get_face_descriptor(img)
-        results[each] = fd
-    return results
-
-
-def get_face_descriptor(img):
-    dets = detector(img, 1)
-    res = []
-    for k, d in enumerate(dets):
-        shape = predictor(img, d)
-        face_descriptor = face_rec.compute_face_descriptor(img, shape)
-        res.append(face_descriptor)
-    return res
+from initialer import FaceImgProcessor
 
 
 def main():
-    video_capture = cv2.VideoCapture(0)
-
+    fip = FaceImgProcessor()
+    # video_capture = cv2.VideoCapture(0)
+    video_capture = cv2.VideoCapture(r'C:\Users\Administrator\OneDrive - TSCN\cgy\机器学习\人脸识别疲劳检测\a.mp4')
     process_this_frame = True
 
     while True:
         ret, frame = video_capture.read()
-
         small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
 
         rgb_small_frame = small_frame[:, :, ::-1]
-
         if process_this_frame:
-            rects = detector(rgb_small_frame, 1)
-            log_msg = []
-            for (i, rect) in enumerate(rects):
+
+            rects = fip.get_locations(rgb_small_frame)
+            landmarks = fip.get_face_landmarks(rgb_small_frame)
+            print(zip(rects,landmarks))
+            for i, (rect, landmark) in enumerate(zip(rects, landmarks)):
                 (x, y, w, h) = face_utils.rect_to_bb(rect)  # 返回人脸框的左上角坐标和矩形框的尺寸
-                x *= 4
-                y *= 4
-                w *= 4
-                h *= 4
-                log_msg.append(f' Face #{i + 1}: ({x},{y})')
-                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-                cv2.putText(frame, "Face #{}".format(i + 1), (x - 10, y - 10),
+                print(i,rect,landmark)
+                # x *= 4
+                # y *= 4
+                # w *= 4
+                # h *= 4
+                cv2.rectangle(small_frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                cv2.putText(small_frame, "Face #{}".format(i + 1), (x - 10, y - 10),
                             cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-            print('|'.join(log_msg))
-        cv2.imshow('Video', frame)
+                for (x, y) in landmark:
+                    # x *= 4
+                    # y *= 4
+                    cv2.circle(frame, (x, y), 1, (255, 0, 0), -1)
+
+        cv2.imshow('Video', small_frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
@@ -87,19 +45,5 @@ def main():
     cv2.destroyAllWindows()
 
 
-def test():
-    img = dlib.load_rgb_image(f'./face2.jpg')
-    win = dlib.image_window()
-    win.clear_overlay()
-    win.set_image(img)
-    dets = detector(img,1)
-
-    for k,d in enumerate(dets):
-        shape = predictor(img, d)
-        face_descriptor = face_rec.compute_face_descriptor(img, shape)
-        print(shape)
-        print(face_descriptor)
-
-
 if __name__ == '__main__':
-    test()
+    main()
