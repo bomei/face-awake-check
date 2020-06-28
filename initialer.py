84efa0fcfe4a913ae6986b863e91a0749f419cb0
zannb <zannb@outlook.com>
2020-06-28 11:33:12 +0800

    use face_recognition directly instead

--- initialer.py
+++ initialer.py
@@ -11,22 +11,20 @@ class FaceImgProcessor:
         pass
 
     @staticmethod
-    def delta_h_eye(shape):
-        left = shape[36][1] + shape[37][1] - shape[39][1] - shape[40][1]
-        right = shape[42][1] + shape[43][1] - shape[46][1] - shape[45][1]
-        return left / 2, right / 2
+    def delta_h_eye(landmark):
+        pass
 
     @staticmethod
-    def delta_w_eye(shape):
-        return shape[38][0] - shape[35][0]
+    def delta_w_eye(landmark):
+        pass
 
     @staticmethod
-    def delta_h_mouth(shape):
-        return (shape[60][1] + shape[61][1] + shape[62][1] - shape[64][1] - shape[65][1] - shape[66][1]) / 3
+    def delta_h_mouth(landmark):
+        pass
 
     @staticmethod
-    def delta_w_mouth(shape):
-        return shape[63][0] - shape[59][0]
+    def delta_w_mouth(landmark):
+        pass
 
     @staticmethod
     def get_locations(image):
