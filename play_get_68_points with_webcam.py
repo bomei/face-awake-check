f9ef66758ab56119521c6912f505f785709caef5
zannb <zannb@outlook.com>
2020-06-28 09:35:01 +0800

    just handle the img without rotation

--- play_get_68_points with_webcam.py
+++ play_get_68_points with_webcam.py
@@ -9,7 +9,7 @@ from initialer import FaceImgProcessor
 def main():
     fip = FaceImgProcessor()
     # video_capture = cv2.VideoCapture(0)
-    video_capture = cv2.VideoCapture(r'C:\Users\Administrator\OneDrive - TSCN\cgy\机器学习\人脸识别疲劳检测\a.mp4')
+    video_capture = cv2.VideoCapture(r'C:\Users\Administrator\OneDrive - TSCN\cgy\机器学习\人脸识别疲劳检测\c.mp4')
     process_this_frame = True
 
     while True:
@@ -21,11 +21,10 @@ def main():
 
             rects = fip.get_locations(rgb_small_frame)
             landmarks = fip.get_face_landmarks(rgb_small_frame)
-            print(zip(rects,landmarks))
-            # TODO: iteration seems not work out
+            print(zip(rects, landmarks))
             for i, (rect, landmark) in enumerate(zip(rects, landmarks)):
-                (x, y, w, h) = face_utils.rect_to_bb(rect)  # 返回人脸框的左上角坐标和矩形框的尺寸
-                print(i,rect,landmark)
+                (x, y, w, h) = rect  # 返回人脸框的左上角坐标和矩形框的尺寸
+                print(i, rect, landmark)
                 # x *= 4
                 # y *= 4
                 # w *= 4
@@ -33,10 +32,10 @@ def main():
                 cv2.rectangle(small_frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
                 cv2.putText(small_frame, "Face #{}".format(i + 1), (x - 10, y - 10),
                             cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-                for (x, y) in landmark:
-                    # x *= 4
-                    # y *= 4
-                    cv2.circle(frame, (x, y), 1, (255, 0, 0), -1)
+                for l,points in landmark.items():
+                    for (x,y) in points:
+                        # y *= 4
+                        cv2.circle(small_frame, (x, y), 5, (255, 0, 0), -1)
 
         cv2.imshow('Video', small_frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
